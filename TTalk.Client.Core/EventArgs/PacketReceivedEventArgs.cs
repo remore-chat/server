275b298f28f6403c26f7d212dae4f774e5093551
roxxel <64275777+roxxel@users.noreply.github.com>
2022-05-22 16:30:17 +0300

    Remove old client code

--- TTalk.Client.Core/EventArgs/PacketReceivedEventArgs.cs
+++
@@ -1,9 +0,0 @@
-﻿using TTalk.Library.Packets;
-
-namespace TTalk.Client.Core.EventArgs
-{
-    public class PacketReceivedEventArgs
-    {
-        public IPacket Packet { get; set; }
-    }
-}
--- TTalk.Client.Example/Program.cs
+++
@@ -1,5 +0,0 @@
-﻿using TTalk.Client.Core;
-
-var ttalkClient = new TTalkClient("127.0.0.1", 9831, username: "roxxelroxx");
-await ttalkClient.ConnectAsync();
-await Task.Delay(-1);
