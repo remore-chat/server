8e986fa1ad0568e13ea8380dd8c5eaff6e09e10c
roxxel <64275777+roxxel@users.noreply.github.com>
2022-06-24 14:46:37 +0300

    Revert "Interim"
    This reverts commit f12647717b05ec60d93072d0df01a0f6a099fcad.

--- TTalk.WinUI/ViewModels/MainViewModel.cs
+++ TTalk.WinUI/ViewModels/MainViewModel.cs
@@ -118,6 +118,7 @@ namespace TTalk.WinUI.ViewModels
                     });
                 }
             });
+            _denoiser = new Denoiser();
             App.MainWindow.DispatcherQueue.TryEnqueue(async () =>
             {
                 IsConnected = true;
@@ -254,39 +255,97 @@ namespace TTalk.WinUI.ViewModels
             EnableRNNoiseSuppression = await SettingsService.ReadSettingAsync<bool>(SettingsViewModel.EnableRNNoiseSuppressionSettingsKey);
         }
 
-        private TTal _client;
+        private TTalkClient _client;
         private CancellationTokenSource _cts;
         private bool? voiceAllowed = null;
-        private int _segmentFrames;
-        private string ip;
-        private int port;
-        public Channel CurrentChannel { get; set; }
         public (string Ip, int Port) ServerAddress => (_client?.Address, _client?.Port ?? 0);
 
         #region Reactive Properties
 
         [ObservableProperty]
         private string serverName;
+
         [ObservableProperty]
         private int serverMaxClients;
-        [ObservableProperty]
+
         private string address;
-        [ObservableProperty]
+
+        public string Address
+        {
+            get { return address; }
+            set { this.SetProperty(ref address, value); }
+        }
+
         private string messageContent;
-        [ObservableProperty]
+
+        public string MessageContent
+        {
+            get { return messageContent; }
+            set { this.SetProperty(ref messageContent, value); }
+        }
+
         private ObservableCollection<Channel> channels;
-        [ObservableProperty]
+
+        public ObservableCollection<Channel> Channels
+        {
+            get { return channels; }
+            set { this.SetProperty(ref channels, value); }
+        }
+
+        private int _segmentFrames;
         private bool isConnected;
-        [ObservableProperty]
+        private string ip;
+        private int port;
+
+        public bool IsConnected
+        {
+            get { return isConnected; }
+            set { this.SetProperty(ref isConnected, value); }
+        }
+
         private Channel currentTextChannel;
-        [ObservableProperty]
+
+        public Channel CurrentTextChannel
+        {
+            get { return currentTextChannel; }
+            set { this.SetProperty(ref currentTextChannel, value); }
+        }
+
+
         private bool isNegotiationFinished;
-        [ObservableProperty]
+
+        public bool IsNegotiationFinished
+        {
+            get { return isNegotiationFinished; }
+            set { SetProperty(ref isNegotiationFinished, value); }
+        }
+
         private bool canEditServerSettings;
-        [ObservableProperty]
+
+        public bool CanEditServerSettings
+        {
+            get { return canEditServerSettings; }
+            set { SetProperty(ref canEditServerSettings, value); }
+        }
+
+
         private ChannelClient currentChannelClient;
-        [ObservableProperty]
+
+        public ChannelClient CurrentChannelClient
+        {
+            get { return currentChannelClient; }
+            set { SetProperty(ref currentChannelClient, value); }
+        }
+
+
+        public Channel CurrentChannel { get; set; }
         private bool isNotConnectingToChannel;
+
+        public bool IsNotConnectingToChannel
+        {
+            get { return isNotConnectingToChannel; }
+            set { this.SetProperty(ref isNotConnectingToChannel, value); }
+        }
 
         public ICommand ShowConnectDialog { get; }
         public RelayCommand DisconnectCommand { get; }
@@ -296,6 +355,8 @@ namespace TTalk.WinUI.ViewModels
         public RelayCommand CreateChannelDialogCommand { get; }
         public RelayCommand<string> DeleteChannelDialogCommand { get; }
         public RelayCommand ShowUpdatePriviligeKeyDialog { get; }
+
+        private Denoiser _denoiser;
 
         public ILookupClient LookupClient { get; }
         public ILocalSettingsService SettingsService { get; }
